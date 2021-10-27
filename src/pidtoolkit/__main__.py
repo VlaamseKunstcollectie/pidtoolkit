2354fa97c16f954c228e68e92cc70aa8553ff6c8
Rob Wyse <rob.wyse@vlaamsekunstcollectie.be>
2021-10-27 09:42:27 +0200

    Remove CultURIze generation.

--- src/pidtoolkit/__main__.py
+++ src/pidtoolkit/__main__.py
@@ -3,24 +3,18 @@
 from argparse import ArgumentParser
 from os import path
 
-from pids.csvgen import generate_pid_csv, generate_culturize_csv
+from pids.csvgen import generate_pid_csv
 
 
 def main(args):
     import_file = args.csv
-    export_directory = args.dest if args.dest else ''
-    pid_export_file = path.join(export_directory, "pids.csv")
-    culturize_export_file = path.join(export_directory, "culturize.csv") if args.urls == "true" else ''
+    export_file = args.dest if args.dest else path.dirname(path.realpath(import_file)) + 'pids.csv'
     config_file = args.config
 
-    generate_pid_csv(import_file, pid_export_file, config_file)
-    print("PIDs exported:", pid_export_file)
+    generate_pid_csv(import_file, export_file, config_file)
+    print("PIDs exported to:", export_file)
 
-    if args.urls == "true":
-        generate_culturize_csv(pid_export_file, culturize_export_file, config_file)
-        print("CultURIze input CSV generated:", culturize_export_file)
-
-    print("Files generated.")
+    print("Finished.")
 
 
 if __name__ == "__main__":
@@ -29,8 +23,6 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--config', required=True,
                         help="Path to generation configuration file. Required.")
     parser.add_argument('-d', '--dest',
-                        help="Path to destination directory")
-    parser.add_argument('-u', '--urls', default="true", choices=['true', 'false'],
-                        help="Generate CSV with URLs for CultURIze. Default 'true'.")
+                        help="Path to destination file")
     args = parser.parse_args()
     main(args)
--- src/pidtoolkit/pids/csvgen.py
+++ src/pidtoolkit/pids/csvgen.py
@@ -4,10 +4,7 @@ import csv
 import json
 from datetime import date
 
-
 from pidgen import replace_unsafe_chars, generate_meemoo_pid, generate_oslo_pid
-from culturize.culturizegen import generate_arthub_pid, generate_datahub_pid, generate_datahub_redirect, \
-    generate_data_redirect
 
 
 def generate_pid_csv(import_file: str, export_file: str, config_file: str):
@@ -98,91 +95,3 @@ def generate_pid_csv(import_file: str, export_file: str, config_file: str):
                     export_row[f"{type}_pid"] = generate_meemoo_pid(pid_number, base_url, pid_concept, type, pid_pattern)
 
             writer.writerow(export_row)
-
-
-# TODO: rewrite for variable PID types
-def generate_culturize_csv(import_file: str, export_file: str, config_file: str):
-    # Import configuration from JSON file
-    config = json.load(config_file)
-
-    # PID-related configuration
-    base_url = config['base_url']
-
-    # CultURIze configuration
-    arthub_url = config['arthub_url']
-    arthub_lang = config['arthub_lang']
-    datahub_url = ['datahub_url']
-    data_to_datahub = ['data_to_datahub']
-
-    csv_contents = csv.DictReader(open(import_file, encoding='utf-8'))
-
-    with open(export_file, 'w') as output_file:
-        fields = [
-            "PID",
-            "URL",
-            "document type",
-            "enabled"
-        ]
-        export_row = {
-            "PID": "",
-            "URL": "",
-            "document type": "",
-            "enabled": "1"
-        }
-        writer = csv.DictWriter(output_file, fieldnames=fields)
-        writer.writeheader()
-        for import_row in csv_contents:
-            pid_number = import_row["websafe_obj_number"]
-
-            arthub_pid = generate_arthub_pid(pid_number, base_url)
-            datahub_pid = generate_datahub_pid(pid_number, base_url, datahub_url)
-
-            export_row["PID"] = pid_number
-
-            export_row["URL"] = import_row['data_pid']
-            export_row["document type"] = "id"
-            writer.writerow(export_row)
-
-            if data_to_datahub:
-                export_row["URL"] = generate_datahub_redirect(datahub_url, datahub_pid)
-            else:
-                export_row["URL"] = generate_data_redirect(arthub_url, arthub_lang, arthub_pid)
-            export_row["document type"] = "data"
-            writer.writerow(export_row)
-
-            # Generating representation redirects in this manner only works
-            # with the non-IIIF Arthub.
-            #
-            # export_row["URL"] = generate_rep_redirect(arthub_url, arthub_lang,
-            #                                           arthub_pid)
-            # export_row["document type"] = "representation"
-            # writer.writerow(export_row)
-
-
-def append_rep_redirects(culturize_csv: str, encoding: str):
-    csvcontents = csv.DictReader(open(culturize_csv, encoding=encoding))
-
-    with open(culturize_csv, 'a') as output_file:
-        fields = [
-            "PID",
-            "URL",
-            "document type",
-            "enabled"
-        ]
-        export_row = {
-            "PID": "",
-            "URL": "",
-            "document type": "",
-            "enabled": "1"
-        }
-        writer = csv.DictWriter(output_file, fieldnames=fields)
-        previous_pid = ""
-        for import_row in csvcontents:
-            if previous_pid == import_row["PID"]:
-                continue
-            export_row["PID"] = import_row["PID"]
-            # TODO: write function to generate the IIIF URLs.
-            #  export_row["URL"] =
-            export_row["document type"] = "representation"
-            writer.writerow(export_row)
-            previous_pid = import_row["PID"]
